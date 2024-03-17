50f66c84b49ab9cf03bb33922245b038afcfb410
Rashnotech <rashnotech@gmail.com>
2024-03-17 17:39:41 +0000

    Fixed: database creation

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- a SQL script that creates a table
 -- users table
-CREATE TABLE IF NOT EXIST (
+CREATE TABLE IF NOT EXIST users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,3 @@
 -- an SQL script that ranks country origins of bands
 -- import metal bands
-
+SELECT origin nb_fans FROM metal_bands ORDER BY ASC fans;
